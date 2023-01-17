773a1da9fb3b0f31e6c8455a14572a10fac80f86
Bikash Shah <bikash.shah@f1soft.com>
2023-01-17 15:57:50 +0545

    using a jdbc user detail management

--- src/main/java/com/bikash/springsecurity/security/SecurityConfig.java
+++ src/main/java/com/bikash/springsecurity/security/SecurityConfig.java
@@ -3,13 +3,12 @@ package com.bikash.springsecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.sql.DataSource;
 
 /**
  * @author Bikash Shah
@@ -18,42 +17,28 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
-//
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        http.httpBasic();
-//        http.authorizeHttpRequests().anyRequest().authenticated();
-//        return http.build();
-//    }
-//
-//
-//    //for custom authentication provider
-//    @Bean
-//    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-//        AuthenticationManagerBuilder authenticationManagerBuilder =
-//                http.getSharedObject(AuthenticationManagerBuilder.class);
-//        authenticationManagerBuilder.authenticationProvider(authProvider);
-//        return authenticationManagerBuilder.build();
-//    }
 
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
+            // or by using query
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails u = new User("bikas", "bikas", "read");
-        UserDetails u1 = new User("nitesh","nitesh","write");
-        List<UserDetails> users = new ArrayList<>();
-        users.add(u);
-        users.add(u1);
-        return new InMemoryUserDetailService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery =
+                "select username, password, enabled from users where username = ?";
+        String authsByUserQuery =
+                "select username, authority from authorities where username = ?";
+
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
     }
+
 }
